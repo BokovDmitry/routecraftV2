4d5079205131050ec026e6031a3a7bfe38fa09c7
Dmytro Bokov <dimabokov5@gmail.com>
2025-05-04 20:49:36 +0100

    vite.congig change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/build/', // ✅ Ensures asset paths are HTTPS-safe relative URLs
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
