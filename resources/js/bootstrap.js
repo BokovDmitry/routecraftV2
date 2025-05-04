5b6c59c0d46375ea146c5c4f2d4ee0e2f67948e2
Dmytro Bokov <dimabokov5@gmail.com>
2025-05-04 21:25:01 +0100

    axios fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
 window.axios = axios;
-
+window.axios.defaults.baseURL = import.meta.env.VITE_API_URL || window.location.origin;
+window.axios.defaults.withCredentials = true;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
