cb0ac668ae6930359ba361409ae6997a292136f7
yakovenkoka <D00272517@student.dkit.ie>
2025-05-07 14:12:32 +0100

    started adding routeDetails

---
+++ public/build/assets/index-DL_hGVFB.js
@@ -0,0 +1,1 @@
+import{g as r,c as t}from"./app-D4-rhsKw.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ resources/js/Pages/RouteDetail.jsx
@@ -0,0 +1,32 @@
+import { usePage } from '@inertiajs/react';
+import { Container } from 'react-bootstrap';
+import Navbar from '../Navbar';
+import '../../css/RouteDetail.css';
+
+export default function RouteDetail({ route }) {
+  const { title, days, description, likes, user, image } = route;
+
+  return (
+    <>
+      <Navbar />
+      <section className="route-detail-section">
+        <Container className="route-detail-container">
+          <h1 className="route-title">{title}</h1>
+          <p className="route-meta">
+            {days} days | by {user?.name ?? 'Unknown Author'}
+          </p>
+
+          <img
+            src={image ? `/storage/${image}` : '/images/default.jpg'}
+            alt={title}
+            className="route-detail-image"
+          />
+
+          <p className="route-description">{description}</p>
+
+          <p className="route-likes"> {likes} likes</p>
+        </Container>
+      </section>
+    </>
+  );
+}
