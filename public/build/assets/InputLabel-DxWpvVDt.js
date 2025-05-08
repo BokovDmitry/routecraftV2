b0baeb0a0cb3a68d0450ba75c0318d6786651296
Dmytro Bokov <dimabokov5@gmail.com>
2025-05-08 05:20:02 +0100

    build

--- public/build/assets/InputLabel-DxWpvVDt.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/InputLabel-DxWpvVDt.js
-import{j as m}from"./app-BUkYBJdy.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
-========
-import{j as m}from"./app-7f27Eirg.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
->>>>>>>> 8faefa9848e15f9fb16044724fe51da639353785:public/build/assets/InputLabel-C_NPS52o.js
