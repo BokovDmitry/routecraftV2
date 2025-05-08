8faefa9848e15f9fb16044724fe51da639353785
yakovenkoka <D00272517@student.dkit.ie>
2025-05-08 05:04:49 +0100

    Added: about us page

--- routes/web.php
+++ routes/web.php
@@ -48,4 +48,9 @@ Route::middleware(['auth'])->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/about', function () {
+    return Inertia::render('AboutUs');
+})->name('about');
+
+
 require __DIR__.'/auth.php';
