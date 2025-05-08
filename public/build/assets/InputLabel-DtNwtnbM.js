40be12779b6459686ebae0ca25188e0e120d7ef9
Dmytro Bokov <dimabokov5@gmail.com>
2025-05-08 05:59:44 +0100

    final build

--- public/build/assets/InputLabel-DtNwtnbM.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/InputLabel-DtNwtnbM.js
-import{j as m}from"./app-B--xmqr1.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
-========
-import{j as m}from"./app-BK9p-bB3.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
->>>>>>>> a0907dd6a6716dfeebfef7945e7adf9e5d63d8e3:public/build/assets/InputLabel-C-DPfCLz.js
--- public/build/assets/index-D_KcFTUn.js
+++
@@ -1,1 +0,0 @@
-import{g as r,h as t}from"./app-BK9p-bB3.js";var a=t();const e=r(a);export{e as R,a as r};
